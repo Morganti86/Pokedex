77300c0491f309fb49cff107fda2eab7bc14ed1c
AlanSeya <Alanseya90@gmail.com>
2023-12-08 13:59:26 +0000

    progress_bar keyframes

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -11,7 +11,9 @@ export function PokemonCard({ pokemon, index }) {
     const handleScroll = () => {
       if (cardRef.current) {
         const rect = cardRef.current?.getBoundingClientRect();
-        const visible = rect?.bottom >= 1.2 && rect?.top <= window.innerHeight / 1.2;
+        console.log("bot: " + rect.bottom);
+        console.log("win: " + window.innerHeight);
+        const visible = rect?.bottom >= 20 && rect?.top <= window.innerHeight / 1.2;
         setIsVisible(visible);
       }
     };
